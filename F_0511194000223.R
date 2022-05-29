f18bb97b0b7ef4ac9e8150e351fb75cdb7abc064
naufalfabian <naufalfabian321@gmail.com>
2022-05-29 23:01:28 +0700

    Update F_0511194000223.R

--- F_0511194000223.R
+++ F_0511194000223.R
@@ -26,12 +26,25 @@ conf.level = 0.95)
 
 
 #soal3
+library(BSDA)
+library(mosaic)
 
 #a
+
 #b
+tsum.test(mean.x=3.64, s.x = 1.67, n.x = 19, 
+          mean.y=2.79, s.y = 1.32, n.y = 27, 
+          alternative = "two.sided", mu = 0, var.equal = TRUE,
+          conf.level = 0.95)
+
 #c
+plotDist(dist ='t', df = 2, col="pink")
+
 #d
+qt(p = 0.05, df = 2, lower.tail = FALSE)
+
 #e
+
 #f
 
 
