b8819cb7382c759ab733bf422a65941e48cef869
naufalfabian <naufalfabian321@gmail.com>
2022-05-29 21:50:01 +0700

    edit

---
+++ F_0511194000223.R
@@ -0,0 +1,72 @@
+#soal1
+
+#a
+x <- c(78,75,67,77,70,72,78,74,77)
+y <- c(100,95,70,90,90,90,89,90,100)
+sd(x-y)
+
+#b
+t.test (y , x , paired=TRUE)
+
+#c
+
+#soal2
+
+#a
+
+#b
+zsum.test(mean.x=23500, sigma.x = 3900, n.x = 100,  
+alternative = "greater", mu = 20000,
+conf.level = 0.95)
+
+#c
+
+#soal3
+
+#a
+#b
+#c
+#d
+#e
+#f
+
+#soal4
+
+#a
+data3<- read.table("https://rstatisticsandresearch.weebly.com/uploads/1/0/2/6/1026585/onewayanova.txt", header= TRUE)
+data3
+
+data3$Group <- as.factor(data3$Group)
+data3$Group = factor(data3$Group,labels = c("grup 1","grup 2", "grup 3"))
+
+Group1 <- subset(data3, Group =="grup 1")
+Group2 <- subset(data3, Group =="grup 2")
+Group3 <- subset(data3, Group =="grup 3")
+
+qqnorm(Group1$Length)
+qqline(Group1$Length)
+
+qqnorm(Group2$Length)
+qqline(Group2$Length)
+
+qqnorm(Group3$Length)
+qqline(Group3$Length)
+
+#b
+bartlett.test(Length ~ Group, data = data3)
+
+#c
+model1 = lm(Length ~ Group, data = data3)
+model1
+
+lm(formula = Length ~ Group, data = data3)
+anova(model1)
+
+#d
+TukeyHSD(aov(model1))
+
+#f
+ggplot(data3, aes( x = Group, y = Length)) +
+  geom_boxplot(fill = "grey80", col = "black") +
+  scale_x_discrete() + xlab("Treatment Group") +
+  ylab("Length (cm")
